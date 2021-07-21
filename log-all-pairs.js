694a719f3c44165c5f293f0f8b083316e07f4e00
dbpatnodee <dbpatnode@hotmail.com>
2021-07-21 15:09:49 -0700

    working through VScode debugger issues

--- log-all-pairs.js
+++ log-all-pairs.js
@@ -2,16 +2,17 @@
 // log all matching pairs in an array
 // this is O(n^2) note that we're using two for loops with different values (i and j) hence n^2
 
-let boxes = [3, 5, 10, 34, 6]
+const boxes = [3, 5, 10, 34, 6]
+const letters = ['a', 'b', 'c', 'd', 'e', 'f']
 
 function matchingPairs(array) {
     // first for loop grabs value of the 1st element(i) of the array.
-      for (let i=0; i < boxes.length; i++) {
+      for (let i=0; i < array.length; i++) {
         // we loop again to grab the next element (j) and console log the two
-          for (let j=i+1; j < boxes.length; j++) {
+          for (let j=i+1; j < array.length; j++) {
             console.log(`${array[i]}  ${array[j]}`)
         }
       }
   }
   
-  matchingPairs(boxes);
+  matchingPairs(letters);
