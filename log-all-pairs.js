7609c5a6f603598cd7d7ec6c35baaa95bf4977cf
dbpatnodee <dbpatnode@hotmail.com>
2021-07-22 12:37:30 -0700

    notes on arrays

--- log-all-pairs.js
+++ log-all-pairs.js
@@ -8,12 +8,31 @@ const letters = ['a', 'b', 'c', 'd', 'e', 'f']
 
 function matchingPairs(array) {
     // first for loop grabs value of the 1st element(i) of the array.
-      for (let i=0; i < array.length; i++) {
+    //   for (let i=0; i < array.length; i++) {
         // we loop again to grab the next element (j) and console log the two
-          for (let j=i+1; j < array.length; j++) {
-            console.log(`${array[i]}  ${array[j]}`)
-        }
-      }
-  }
+        //   for (let j=i+1; j < array.length; j++) {
+        //     console.log(`${array[i]}  ${array[j]}`)
+        // }
+    //   }
+
+    // ES5 syntax
+    // array.forEach(function(firstPartnerInPair) {
+    //     array.forEach(function (secondPartnerInPair) {
+    //         console.log(`${firstPartnerInPair} ${secondPartnerInPair}`)
+    //     })
+    // })
+    // ES6
+
+    array.forEach((firstPartnerInPair) => {
+        
+        array.forEach((secondPartnerInPair) => {
+            // console.log(secondPartnerInPair)
+            // console.log(`${firstPartnerInPair} ${secondPartnerInPair}`)
+        })
+    })
+
+}
+
+    
   
   matchingPairs(letters);
