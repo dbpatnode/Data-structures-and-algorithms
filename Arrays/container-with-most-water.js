8b20a55f6ee6efe79f712413a56e50df22f6caa0
dbpatnodee <dbpatnode@hotmail.com>
2021-07-23 13:54:03 -0700

    set up for container with most water

---
+++ Arrays/container-with-most-water.js
@@ -0,0 +1,30 @@
+// CONTAINER WITH MOST WATER
+    // you are given an array of positive integers where each integer represents the height of a vertical line on a chart
+    // find two lines which together with the x-axis  forms a  container that would hold the greatest amount of water.
+    // return the area of water it would hold on
+
+// QUESTIONS:
+    //  does the thickness of the lines matter?
+    // do the left and right sides count as walls?
+    // will a higher line within the container affect the area within the container?
+
+
+    lineValues0 = [10, 1, 1, 1, 10]
+
+    lineValues1 = []
+
+    lineValues2 = [1, 2, 3, 4, 5, 6]
+
+    lineValues3 = [10, 50, 100, 15]
+
+    lineValues4 = [5, 5, 5, 5]
+
+
+
+
+    function containerWithMostWater(line) {
+
+    }
+
+    console.log(containerWithMostWater(lineValues2))
+    
