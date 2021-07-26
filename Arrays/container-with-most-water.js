b833965d1cfe3d49320ec0f22a362e9b57e5e44f
dbpatnodee <dbpatnode@hotmail.com>
2021-07-26 15:41:10 -0700

    full break down of logic

--- Arrays/container-with-most-water.js
+++ Arrays/container-with-most-water.js
@@ -10,7 +10,7 @@
 
 
 
-    lineValues0 = [10, 1, 1, 1, 10]
+    lineValues0 = [10, 1, 1, 1, 9]
 
     lineValues1 = []
 
@@ -27,6 +27,11 @@
         // area = L x W
         // L will be equal to the lines.length
         // and width will be equal to the shorter of the two values
+        // in an array like so: [1, 10, 1, 9] we'd need to calculate the min width: min(a, b) where a is the 10 value and b is the 9
+        // then multiply it by the length which is calculated by getting the length subtracting the index of the right most value(10) from the index of the left most value(9).
+        // the resulting formula would look like this:
+        // min(a, b) x (indexOf(b) - indexOf(a))
+        // we'll need to calculate this for every pair because we need to find the largest possible outcome
     }
 
     console.log(containerWithMostWater(lineValues2))
